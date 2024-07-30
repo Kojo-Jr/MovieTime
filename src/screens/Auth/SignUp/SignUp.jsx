69e4b4f9bf24fcf81764e2158e381da75e4fce07
Kojo-Jr <omarieben7@gmail.com>
2024-07-30 17:48:47 +0000

    updates

--- src/screens/Auth/SignUp/SignUp.jsx
+++ src/screens/Auth/SignUp/SignUp.jsx
@@ -1,10 +1,35 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 
 const SignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={{ flex: 1, padding: wp("3") }}>
+      <StatusBar style="auto" />
+      <View
+        style={{
+          marginTop: wp("20"),
+          alignItems: "center"
+        }}
+      >
+        <View>
+          <Text
+            style={{
+              fontSize: wp("7"),
+              fontWeight: "semibold",
+              fontFamily: "Roboto"
+            }}
+          >
+            Welcome to Movietime!
+          </Text>
+        </View>
+        <View>
+          <Text style={{ fontFamily: "Roboto" }}>
+            Your personal guide to the world of Cinema
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Auth/SplashScreen/SplashScreen.jsx
+++ src/screens/Auth/SplashScreen/SplashScreen.jsx
@@ -1,24 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 
 const SplashScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.splashscreenContainer}>
       <StatusBar style="auto" />
-      <Text
-        style={{
-          fontSize: wp("10"),
-          fontFamily: "Roboto",
-          fontWeight: "bold",
-          color: "#FB5558"
-        }}
-      >
-        movietime
-      </Text>
+      <Text style={styles.splashScreenText}>movietime</Text>
     </View>
   );
 };
 
 export default SplashScreen;
+
+const styles = StyleSheet.create({
+  splashscreenContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  splashScreenText: {
+    fontSize: wp("10"),
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    color: "#FB5558"
+  }
+});
