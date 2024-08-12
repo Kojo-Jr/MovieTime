41cabe7d4b49b53e0b72a5b2b2bf81af84fbc17e
kojo-jr <omarieben@outlook.com>
2024-08-12 08:59:24 +0000

    updates

--- src/navigations/DrawerNavigator/DrawerNavigator.jsx
+++ src/navigations/DrawerNavigator/DrawerNavigator.jsx
@@ -5,6 +5,9 @@ import HomeStackNavigator from "../Protected/HomeStack/HomeStackNavigator";
 import ProfileScreen from "../../screens/Profile/ProfileScreen";
 import NewsScreen from "../../screens/NewsScreen/NewsScreen";
 import RecommendationScreen from "../../screens/Recommendations/RecommendationScreen";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FavouriteScreen from "../../screens/Favourite/FavouriteScreen";
+import SettingsScreen from "../../screens/Settings/SettingsScreen";
 
 const Drawer = createDrawerNavigator();
 const DrawerNavigator = () => {
@@ -13,22 +16,50 @@ const DrawerNavigator = () => {
       <Drawer.Screen
         name="Home"
         component={HomeStackNavigator}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          drawerIcon: () => <AntDesign name="home" size={24} color="black" />
+        }}
       />
       <Drawer.Screen
         name="Profile"
         component={ProfileScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          drawerIcon: () => <AntDesign name="user" size={24} color="black" />
+        }}
       />
       <Drawer.Screen
         name="News"
         component={NewsScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          drawerIcon: () => <AntDesign name="book" size={24} color="black" />
+        }}
+      />
+      <Drawer.Screen
+        name="Favourite"
+        component={FavouriteScreen}
+        options={{
+          headerShown: false,
+          drawerIcon: () => <AntDesign name="hearto" size={24} color="black" />
+        }}
       />
       <Drawer.Screen
         name="Recommendation"
         component={RecommendationScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          drawerIcon: () => <AntDesign name="staro" size={24} color="black" />
+        }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          headerShown: false,
+          drawerIcon: () => <AntDesign name="setting" size={24} color="black" />
+        }}
       />
     </Drawer.Navigator>
   );
