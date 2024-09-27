b06a15906e19a04a7a4e8a08e4aa2f4be126d9bc
kojo-jr <omarieben7@gmail.com>
2024-09-27 00:46:46 +0000

    updates

--- src/navigations/DrawerNavigator/DrawerNavigator.jsx
+++ src/navigations/DrawerNavigator/DrawerNavigator.jsx
@@ -1,4 +1,3 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeStackNavigator from "../Protected/HomeStack/HomeStackNavigator";
